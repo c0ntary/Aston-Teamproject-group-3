adda22274a4652e6c124af6679b1fc4c9f970008
c0ntary <contary01@gmail.com>
2026-03-22 16:45:03 +0300

    добавил рандома в email

--- src/main/java/org/example/Data/RandomData.java
+++ src/main/java/org/example/Data/RandomData.java
@@ -8,24 +8,30 @@ import java.util.stream.IntStream;
 
 public class RandomData implements Data {
 
-    private static final String[] NAMES = {"Иван", "Петя", "Вася", "Аня", "Оля"};
+    private static final String[] NAMES = {"Иван", "Петя", "Вася", "Аня", "Оля", "Вика"};
+    private static final String[] NAMES2 = {"CyberKnight", "MoonlightFox", "NeonStorm", "ShadowWalker", "CrimsonEagle", "PhantomByte"};
     private final Random random = new Random();
 
     @Override
     public List<User> loadData(int count) {
         return IntStream.range(0, count)
-                .mapToObj(i -> new User.builder()
-                        .setName(randomName())
-                        .setEmail(randomEmail())
-                        .setId(randomId())
-                        .build())
+                .mapToObj(i -> {
+                    String name = randomName();
+                    String Name2 = randomName2();
+                    String email = Name2.toLowerCase() + random.nextInt(10000) + "@mail.ru";
+                    return new User.builder()
+                            .setName(name)
+                            .setEmail(email)
+                            .setId(randomId())
+                            .build();
+                })
                 .collect(Collectors.toList());
     }
     private String randomName() {
         return NAMES[random.nextInt(NAMES.length)];
     }
-    private String randomEmail() {
-        return randomName().toLowerCase() + "@mail.ru";
+    private String randomName2() {
+        return NAMES2[random.nextInt(NAMES2.length)];
     }
     private long randomId() {
         long min = 1_000_000_000L;
